f5dde8f1f1f8c1a8edd34f638e012e67f6fbde32
Yogesh Naik <yogeshrnaik@gmail.com>
2017-01-21 12:42:51 +0530

    week 02 - homework 2.4 solution

--- MongoUniversity/M101J/week-02/create_blog_authors/src/main/java/course/UserDAO.java
+++ MongoUniversity/M101J/week-02/create_blog_authors/src/main/java/course/UserDAO.java
@@ -1,27 +1,14 @@
 /*
- * Copyright 2015 MongoDB, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2015 MongoDB, Inc. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package course;
 
-import com.mongodb.ErrorCategory;
-import com.mongodb.MongoWriteException;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import sun.misc.BASE64Encoder;
+import static com.mongodb.client.model.Filters.eq;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
@@ -29,9 +16,17 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
 
-import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+
+import sun.misc.BASE64Encoder;
+
+import com.mongodb.ErrorCategory;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class UserDAO {
+
     private final MongoCollection<Document> usersCollection;
     private Random random = new SecureRandom();
 
@@ -48,15 +43,18 @@ public class UserDAO {
         // create an object suitable for insertion into the user collection
         // be sure to add username and hashed password to the document. problem instructions
         // will tell you the schema that the documents must follow.
+        Document user = new Document("_id", username).append("password", passwordHash);
 
         if (email != null && !email.equals("")) {
             // XXX WORK HERE
             // if there is an email address specified, add it to the document too.
+            user.append("email", email);
         }
 
         try {
             // XXX WORK HERE
             // insert the document into the user collection here
+            usersCollection.insertOne(user);
             return true;
         } catch (MongoWriteException e) {
             if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
@@ -72,6 +70,8 @@ public class UserDAO {
 
         // XXX look in the user collection for a user that has this username
         // assign the result to the user variable.
+
+        user = usersCollection.find(eq("_id", username)).first();
 
         if (user == null) {
             System.out.println("User not in database");
@@ -90,14 +90,13 @@ public class UserDAO {
         return user;
     }
 
-
     private String makePasswordHash(String password, String salt) {
         try {
             String saltedAndHashed = password + "," + salt;
             MessageDigest digest = MessageDigest.getInstance("MD5");
             digest.update(saltedAndHashed.getBytes());
             BASE64Encoder encoder = new BASE64Encoder();
-            byte hashedBytes[] = (new String(digest.digest(), "UTF-8")).getBytes();
+            byte hashedBytes[] = new String(digest.digest(), "UTF-8").getBytes();
             return encoder.encode(hashedBytes) + "," + salt;
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("MD5 is not available", e);
