b31d2462a448423f96c3b947caa459a8c1c0e56d
Yogesh Naik <yogeshrnaik@gmail.com>
2017-01-25 20:18:05 +0530

    homework 3.2 and 3.3

--- MongoUniversity/M101J/week-03/blog_accepting_posts_and_comments/src/main/java/course/BlogPostDAO.java
+++ MongoUniversity/M101J/week-03/blog_accepting_posts_and_comments/src/main/java/course/BlogPostDAO.java
@@ -1,12 +1,19 @@
 package course;
 
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
+
 public class BlogPostDAO {
+
     MongoCollection<Document> postsCollection;
 
     public BlogPostDAO(final MongoDatabase blogDatabase) {
@@ -16,10 +23,9 @@ public class BlogPostDAO {
     // Return a single post corresponding to a permalink
     public Document findByPermalink(String permalink) {
 
-        // XXX HW 3.2,  Work Here
-        Document post = null;
-
-
+        // XXX HW 3.2, Work Here
+        MongoCursor<Document> posts = postsCollection.find(Filters.eq("permalink", permalink)).limit(1).iterator();
+        Document post = posts.hasNext() ? posts.next() : null;
 
         return post;
     }
@@ -28,13 +34,12 @@ public class BlogPostDAO {
     // how many posts are returned.
     public List<Document> findByDateDescending(int limit) {
 
-        // XXX HW 3.2,  Work Here
+        // XXX HW 3.2, Work Here
         // Return a list of DBObjects, each one a post from the posts collection
-        List<Document> posts = null;
+        List<Document> posts = postsCollection.find().sort(Sorts.descending("date")).limit(limit).into(new ArrayList<Document>());
 
         return posts;
     }
-
 
     public String addPost(String title, String body, List tags, String username) {
 
@@ -43,7 +48,6 @@ public class BlogPostDAO {
         String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
         permalink = permalink.replaceAll("\\W", ""); // get rid of non alphanumeric
         permalink = permalink.toLowerCase();
-
 
         // XXX HW 3.2, Work Here
         // Remember that a valid post has the following keys:
@@ -57,31 +61,35 @@ public class BlogPostDAO {
 
         // Build the post object and insert it
         Document post = new Document();
-
+        post.append("title", title)
+            .append("author", username)
+            .append("body", body)
+            .append("permalink", permalink)
+            .append("tags", tags)
+            .append("comments", new ArrayList<Document>())
+            .append("date", new Date());
+        postsCollection.insertOne(post);
 
         return permalink;
     }
 
-
-
-
     // White space to protect the innocent
-
-
-
-
-
-
-
 
     // Append a comment to a blog post
     public void addPostComment(final String name, final String email, final String body,
-                               final String permalink) {
+        final String permalink) {
 
         // XXX HW 3.3, Work Here
         // Hints:
         // - email is optional and may come in NULL. Check for that.
         // - best solution uses an update command to the database and a suitable
-        //   operator to append the comment on to any existing list of comments
+        // operator to append the comment on to any existing list of comments
+        Document comment = new Document();
+        comment.append("author", name).append("body", body);
+        if (email != null) {
+            comment.append("email", email);
+        }
+
+        postsCollection.updateOne(Filters.eq("permalink", permalink), new Document("$push", new Document("comments", comment)));
     }
 }
